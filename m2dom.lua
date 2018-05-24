8c15c086363077ab0cb9c99d7f4448e86a90ef81
maanlamp <32102935+maanlamp@users.noreply.github.com>
2018-05-24 23:23:47 +0200

    made progress yeah

--- m2dom.lua
+++ m2dom.lua
@@ -1,37 +1,61 @@
+--[[
+	declare a function with "function" or "ƒ"
+	datatypes:
+	-short        | num    | int     | flo     | str    | fun      | boo      | nul  | obj    | arr
+	-intermediate |        |         | float   |        | func     | bool     |      |        |    
+	-verbose      | number | integer | decimal | string | function | boolean  | null | object | array
+]]--
+
 local toBeCompiled = [[
-	const a = 13;
-	local b = 12;
-	function a () {
-		return "peter";
+	const int: a = 13;
+	local int: b = 12;
+	function a (str: text) {
+		local namen = ["Gert", "Harry", "Frank"];
+		return `${text} namen[3]`;
 	}
+	local obj: gert = {
+		gay: true;
+	};
+	local frank = (gert.gay) ? "Gerts vriendje" : "Eenzaam";
 ]];
 
-local function Scope ()
-	return {
-		constants = {},
-		locals = {},
-		functions = {},
-		expressions = {},
-		scopes = {}
-	};
+local M = {};
+
+function M:expressions (str)
+	str = str:gsub("%c", "");
+	local currentScope = {};
+	local lexed = {};
+	local blocks = {};
+
+	--Save and splice all blocks to be parsed later
+	for block in str:gmatch("%b{}") do
+		table.insert(blocks, block);
+		str = str:gsub(block:gsub("[%(%)%.%%%+%-%*%?%[%]%^%$]", "%%%0"), "###"..#blocks.."###;"):gsub(";+", ";");
+	end
+
+	--Save all expressions in current scope.
+	for expression in str:gmatch("[_%w%p ]-;") do
+		table.insert(currentScope, expression);
+	end
+
+	for i in ipairs(currentScope) do
+		if currentScope[i] == currentScope[i]:gsub("###(%d-)###", "") then
+			currentScope[i] = {
+				value = currentScope[i],
+				block = nil
+			};
+		else
+			currentScope[i] = {
+				value = currentScope[i]:gsub("###(%d-)###", ""),
+				block = currentScope[i]:gsub("(.*)###(%d-)###", function (str, i) return blocks[tonumber(i)] end)
+			};
+		end
+	end
+
+	return currentScope;
 end
 
-local function compile (str)
-	--Clean str
-	str = "\n"..str:gsub("%c", "").."\n";
-	--Declare DOM to be a scope.
-	local dom = Scope();
-	--Find all expressions, and save them.
-	for expression in str:gmatch("\n([^%c;]+)\n") do
-		dom.expressions:insert(expression);
-	end
-	return dom;
-end
-
-local dom = compile(toBeCompiled);
-for k, v in pairs(dom) do
-	print(k..":");
-	for kk, vk in pairs(v) do
-		print("  "..kk..": "..vk);
-	end
+local expressions = M:expressions(toBeCompiled);
+for i, v in ipairs(expressions) do
+	print("Value: "..v.value.."\nBlock: "..(v.block or "nil").."\n");
 end
