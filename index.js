da56c4c7d3231efd91e67fe98f4e1c7913dd938c
maanlamp <32102935+maanlamp@users.noreply.github.com>
2018-05-31 17:07:27 +0200

    Fixed little big issue

--- index.js
+++ index.js
@@ -3,11 +3,11 @@ const { AbstractSyntaxTree } = require("./Classes/AbstractSyntaxTree");
 const { AnnotatedSyntaxTree } = require("./Classes/AnnotatedSyntaxTree");
 const fs = require("fs");
 
-const oddLang = new Map(
+const oddLang = new Map([
 	["declaration", /\bstatic\b|\blocal\b|\bglobal\b|\bconst\b|\bpublic\b|\bdefine\b|\bƒ\b|\bfunction\b/],
 	["operator",    /\breturn\b|\bnew\b|\bdelete\b|\btypeof\b|\binstanceof|\bin\b/],
 	["type",        /\bany\b|\bnum\b|\bint\b|\bdec\b|\bstr\b|\bfun\b|\bfnc\b|\bbool\b|\bboo\b|\bnull\b|\bnul\b|\bnil\b|\bobj\b|\barr\b/]
-);
+]);
 
 let timestamps = [new Date()];
 function timeSince () {
@@ -32,7 +32,7 @@ function compile (from, to) {
 		const annotatedSyntaxTree = new AnnotatedSyntaxTree(abstractSyntaxTree);
 		console.log(`Generated annotated syntax tree in ${timeSince()}ms.\n`);
 		
-		console.log(abstractSyntaxTree.nodes);
+		console.log(abstractSyntaxTree.nodes.map(el => el.value));
 
 		if (!to) process.exit();
 		console.log(`Writing ${to}...`);
@@ -45,4 +45,4 @@ function compile (from, to) {
 	});
 }
 
-compile(`${__dirname}/m/test.txt`);//, `${__dirname}/lexed/test.json`);
+compile(`${__dirname}/odd/test.odd`);//, `${__dirname}/lexed/test.json`);
