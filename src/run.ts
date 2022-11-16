8635860efb4e33bb82355969b70347a637b5f0b8
maanlamp <32102935+maanlamp@users.noreply.github.com>
2022-11-16 16:11:20 +0100

    Bump version

--- src/run.ts
+++ src/run.ts
@@ -16,7 +16,7 @@ const compile = async (target: string) => {
 
 const repl = async () => {
   process.stdin.setEncoding("utf-8");
-  process.stdout.write(`Odd v0.2.2 repl\n> `);
+  process.stdout.write(`Odd v0.2.3 repl\n> `);
   let env: Env = defaultEnv;
   for await (const input of process.stdin) {
     try {
