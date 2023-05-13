e937beb6b7a6207ff73ae3f7d327779c8057256d
maanlamp <32102935+maanlamp@users.noreply.github.com>
2023-05-14 01:23:25 +0200

    Expand declaration tests

--- src/test/declaration.ts
+++ src/test/declaration.ts
@@ -2,34 +2,44 @@ import _eval from "../core/eval.js";
 import parse, { defaultEnv } from "../core/odd.js";
 import test from "../core/test.js";
 import {
+  booleanType,
+  defaultTypeEnv,
+  infer,
+  numberType,
+  stringify,
+} from "../core/type.js";
+import {
   diff,
-  difference,
   equal,
   serialise,
 } from "../core/util.js";
 
 test("Declared values are stored in scope", () => {
   const code = `a = 1`;
-  const [, , env] = _eval(
-    parse(code),
-    defaultEnv,
-    code
-  );
+  const parsed = parse(code);
+  const [, , env] = _eval(parsed, defaultEnv, code);
+  const [, tenv] = infer(parsed, defaultTypeEnv, code);
 
   if (env["a"] !== 1)
     return `Expected 1 but got ${env["a"]}`;
+  if (tenv["a"] !== numberType)
+    return `Expected Number but got ${stringify(
+      env["a"]
+    )}`;
 });
 
 test("Declarations evaluate to rhs", () => {
   const code = `a = 1`;
-  const [result] = _eval(
-    parse(code),
-    defaultEnv,
-    code
-  );
+  const parsed = parse(code);
+  const [result] = _eval(parsed, defaultEnv, code);
+  const [type] = infer(parsed, defaultTypeEnv, code);
 
   if (result !== 1)
     return `Expected 1 but got ${result}`;
+  if (type !== numberType)
+    return `Expected Number but got ${stringify(
+      type
+    )}`;
 });
 
 test("Function declarations are desugared into lambdas", () => {
@@ -48,14 +58,16 @@ test("Function declarations are desugared into lambdas", () => {
 
 test("Custom infix operators", () => {
   const code = `a %^& b = 7;1 %^& 3`;
-  const [result] = _eval(
-    parse(code),
-    defaultEnv,
-    code
-  );
+  const parsed = parse(code);
+  const [result] = _eval(parsed, defaultEnv, code);
+  const [type] = infer(parsed, defaultTypeEnv, code);
 
   if (result !== 7)
     return `Expected 7 but got ${result}`;
+  if (type !== numberType)
+    return `Expected Number but got ${stringify(
+      type
+    )}`;
 });
 
 test("Custom infix operators preserve environment", () => {
@@ -63,21 +75,20 @@ test("Custom infix operators preserve environment", () => {
 		a %^& b = 7;
 		1 %^& 3;
 	`;
-  const [, , env] = _eval(
-    parse(code),
-    defaultEnv,
-    code
-  );
+  const parsed = parse(code);
+  const [, , env] = _eval(parsed, defaultEnv, code);
+  const [type] = infer(parsed, defaultTypeEnv, code);
 
-  if (
-    typeof difference(env, defaultEnv)["%^&"] !==
-    "function"
-  )
+  if (typeof env["%^&"] !== "function")
     return `Operator %^& was not defined or is not a function`;
   if (
     !equal(env, defaultEnv, ([key]) => key !== "%^&")
   )
     return `Environment was altered outside of %^&`;
+  if (type !== numberType)
+    return `Expected Number but got ${stringify(
+      type
+    )}`;
 });
 
 test("Infix declarations are desugared into lambdas", () => {
@@ -99,85 +110,140 @@ test("Infix declarations allow arbitrary patterns", () => {
 		{a} %^& [b] = a + b;
 		{a=1} %^& [2];
 	`;
-  const [result] = _eval(
-    parse(code),
-    defaultEnv,
-    code
-  );
+  const parsed = parse(code);
+  const [result] = _eval(parsed, defaultEnv, code);
+  const [type] = infer(parsed, defaultTypeEnv, code);
 
   if (result !== 3)
     return `Expected 3 but got ${result}`;
+  if (type !== numberType)
+    return `Expected Number but got ${stringify(
+      type
+    )}`;
 });
 
 test("Nth-order list pattern destructuring", () => {
   const code = `[a,[b,[c,de]]]=[1,[2,[3,[4, 5]]]];`;
-  const [, , env] = _eval(
-    parse(code),
-    defaultEnv,
-    code
-  );
+  const parsed = parse(code);
+  const [, , env] = _eval(parsed, defaultEnv, code);
+  const [, tenv] = infer(parsed, defaultTypeEnv, code);
 
   if (env["a"] !== 1)
     return `Expected 1 but got ${env["a"]}`;
+  if (tenv["a"] !== numberType)
+    return `Expected Number but got ${stringify(
+      tenv["a"]!
+    )}`;
   if (env["b"] !== 2)
     return `Expected 2 but got ${env["b"]}`;
+  if (tenv["b"] !== numberType)
+    return `Expected Number but got ${stringify(
+      tenv["b"]!
+    )}`;
   if (env["c"] !== 3)
     return `Expected 3 but got ${env["c"]}`;
+  if (tenv["c"] !== numberType)
+    return `Expected Number but got ${stringify(
+      tenv["c"]!
+    )}`;
   if (!equal(env["de"], [4, 5]))
     return `Expected [4, 5] but got ${serialise(
       env["de"]
+    )}`;
+  if (stringify(tenv["de"]!) !== "List Number")
+    return `Expected List Number but got ${stringify(
+      tenv["de"]!
     )}`;
 });
 
 test("Nth-order record pattern destructuring", () => {
   const code = `{a,b={c,d={e}}}={a=1,b={c=2,d={e=3}}};`;
-  const [, , env] = _eval(
-    parse(code),
-    defaultEnv,
-    code
-  );
+  const parsed = parse(code);
+  const [, , env] = _eval(parsed, defaultEnv, code);
+  const [, tenv] = infer(parsed, defaultTypeEnv, code);
 
   if (env["a"] !== 1)
     return `Expected 1 but got ${env["a"]}`;
+  if (tenv["a"] !== numberType)
+    return `Expected Number but got ${stringify(
+      tenv["a"]!
+    )}`;
   if (env["c"] !== 2)
     return `Expected 2 but got ${env["c"]}`;
+  if (tenv["c"] !== numberType)
+    return `Expected Number but got ${stringify(
+      tenv["c"]!
+    )}`;
   if (env["e"] !== 3)
     return `Expected 3 but got ${env["e"]}`;
+  if (tenv["e"] !== numberType)
+    return `Expected Number but got ${stringify(
+      tenv["e"]!
+    )}`;
 });
 
 test("List destructuring rest pattern", () => {
-  const code = `[[a,...b]]=[[1, 2, 3]];[a,b]`;
-  const [result] = _eval(
-    parse(code),
-    defaultEnv,
-    code
-  );
+  const code = `[a,...b]=[1, 2, 3];`;
+  const parsed = parse(code);
+  const [, , env] = _eval(parsed, defaultEnv, code);
+  const [, tenv] = infer(parsed, defaultTypeEnv, code);
 
-  if (!equal(result, [1, [2, 3]]))
-    return `Expected [1, [2, 3]] but got ${serialise(
-      result
+  if (env["a"] !== 1)
+    return `Expected 1 but got ${serialise(env["a"])}`;
+  if (tenv["a"] !== numberType)
+    return `Expected Number but got ${stringify(
+      tenv["a"]!
+    )}`;
+  if (!equal(env["b"], [2, 3]))
+    return `Expected [2, 3] but got ${serialise(
+      env["b"]
+    )}`;
+  if (stringify(tenv["b"]!) !== "List Number")
+    return `Expected List Number but got ${stringify(
+      tenv["b"]!
     )}`;
 });
 
 test("Record destructuring rest pattern", () => {
   const code = `{a,b,c,d,...x}={a=1,b=2,c=3,d=4,y=5,z=6}`;
-  const [, , env] = _eval(
-    parse(code),
-    defaultEnv,
-    code
-  );
+  const parsed = parse(code);
+  const [, , env] = _eval(parsed, defaultEnv, code);
+  const [, tenv] = infer(parsed, defaultTypeEnv, code);
 
   if (env["a"] !== 1)
     return `Expected 1 but got ${env["a"]}`;
+  if (tenv["a"] !== numberType)
+    return `Expected Number but got ${stringify(
+      tenv["a"]!
+    )}`;
   if (env["b"] !== 2)
     return `Expected 2 but got ${env["b"]}`;
+  if (tenv["b"] !== numberType)
+    return `Expected Number but got ${stringify(
+      tenv["b"]!
+    )}`;
   if (env["c"] !== 3)
     return `Expected 3 but got ${env["c"]}`;
+  if (tenv["c"] !== numberType)
+    return `Expected Number but got ${stringify(
+      tenv["c"]!
+    )}`;
   if (env["d"] !== 4)
-    return `Expected 3 but got ${env["d"]}`;
+    return `Expected 4 but got ${env["d"]}`;
+  if (tenv["d"] !== numberType)
+    return `Expected Number but got ${stringify(
+      tenv["d"]!
+    )}`;
   if (!equal(env["x"], { y: 5, z: 6 }))
     return `Expected { y: 5, z: 6 } but got ${serialise(
       env["x"]
+    )}`;
+  if (
+    stringify(tenv["a"]) !==
+    "{ y : Number, z : Number }"
+  )
+    return `Expected { y : Number, z : Number } but got ${stringify(
+      tenv["a"]
     )}`;
 });
 
@@ -187,14 +253,16 @@ test("Self recursion", () => {
 			true = n,
 			false = fib (n - 2) + fib (n - 1);
 		fib 10`;
-  const [result] = _eval(
-    parse(code),
-    defaultEnv,
-    code
-  );
+  const parsed = parse(code);
+  const [result] = _eval(parsed, defaultEnv, code);
+  const [type] = infer(parsed, defaultTypeEnv, code);
 
   if (result !== 55)
     return `Expected 55 but got ${result}`;
+  if (type !== numberType)
+    return `Expected Number but got ${stringify(
+      type
+    )}`;
 });
 
 test("Mutual recursion", () => {
@@ -207,14 +275,16 @@ test("Mutual recursion", () => {
 			false = is-odd (n - 1);
 		is-odd 3;
 	`;
-  const [result] = _eval(
-    parse(code),
-    defaultEnv,
-    code
-  );
+  const parsed = parse(code);
+  const [result] = _eval(parsed, defaultEnv, code);
+  const [type] = infer(parsed, defaultTypeEnv, code);
 
   if (result !== true)
     return `Expected true but got ${result}`;
+  if (type !== booleanType)
+    return `Expected Number but got ${stringify(
+      type
+    )}`;
 });
 
 test("Curried declarations are folded properly", () => {
