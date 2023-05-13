85340d08e9635c8be49ff569cc7e64ed3dddcfcb
maanlamp <32102935+maanlamp@users.noreply.github.com>
2023-05-14 01:00:27 +0200

    Extend application tests

--- src/test/application.ts
+++ src/test/application.ts
@@ -2,6 +2,12 @@ import _eval from "../core/eval.js";
 import parse, { defaultEnv } from "../core/odd.js";
 import test from "../core/test.js";
 import {
+  defaultTypeEnv,
+  infer,
+  numberType,
+  stringify,
+} from "../core/type.js";
+import {
   diff,
   equal,
   serialise,
@@ -9,77 +15,81 @@ import {
 
 test("Function application", () => {
   const code = `f x=x+1;f 0`;
-  const [result] = _eval(
-    parse(code),
-    defaultEnv,
-    code
-  );
+  const parsed = parse(code);
+  const [result] = _eval(parsed, defaultEnv, code);
+  const [type] = infer(parsed, defaultTypeEnv, code);
 
   if (result !== 1)
     return `Expected 1 but got ${result}`;
+  if (type !== numberType)
+    return `Expected Number but got ${stringify(
+      type
+    )}`;
 });
 
 test("Literal application", () => {
   const code = `(x -> x + 1) 0`;
-  const [result] = _eval(
-    parse(code),
-    defaultEnv,
-    code
-  );
+  const parsed = parse(code);
+  const [result] = _eval(parsed, defaultEnv, code);
+  const [type] = infer(parsed, defaultTypeEnv, code);
 
   if (result !== 1)
     return `Expected 1 but got ${result}`;
+  if (type !== numberType)
+    return `Expected Number but got ${stringify(
+      type
+    )}`;
 });
 
 test("Record access", () => {
   const code = `{a=1} ''a''`;
-  const [result] = _eval(
-    parse(code),
-    defaultEnv,
-    code
-  );
+  const parsed = parse(code);
+  const [result] = _eval(parsed, defaultEnv, code);
+  const [type] = infer(parsed, defaultTypeEnv, code);
 
   if (result !== 1)
     return `Expected 1 but got ${result}`;
+  if (type !== numberType)
+    return `Expected Number but got ${stringify(
+      type
+    )}`;
 });
 
 test("List access", () => {
   const code = `[1] 0`;
-  const [result] = _eval(
-    parse(code),
-    defaultEnv,
-    code
-  );
+  const parsed = parse(code);
+  const [result] = _eval(parsed, defaultEnv, code);
+  const [type] = infer(parsed, defaultTypeEnv, code);
 
   if (result !== 1)
     return `Expected 1 but got ${result}`;
+  if (type !== numberType)
+    return `Expected Number but got ${stringify(
+      type
+    )}`;
 });
 
 test("Application has higher precedence than infix", () => {
   const code = `f x=x;f 1 + f 1`;
-  const [result] = _eval(
-    parse(code),
-    defaultEnv,
-    code
-  );
+  const parsed = parse(code);
+  const [result] = _eval(parsed, defaultEnv, code);
+  const [type] = infer(parsed, defaultTypeEnv, code);
 
   if (result !== 2)
     return `Expected 2 but got ${result}`;
+  if (type !== numberType)
+    return `Expected Number but got ${stringify(
+      type
+    )}`;
 });
 
 test("Record access and application", () => {
-  const code1 = `numbers ''a'' + numbers ''b'';`;
-  const code2 = `(numbers ''a'') + (numbers ''b'');`;
+  const code1 = `numbers={a=1,b=1};numbers ''a'' + numbers ''b'';`;
+  const code2 = `numbers={a=1,b=1};(numbers ''a'') + (numbers ''b'');`;
   const tree1 = parse(code1);
   const tree2 = parse(code2);
-  const [result] = _eval(
-    tree1,
-    {
-      ...defaultEnv,
-      numbers: { a: 1, b: 1 },
-    },
-    code1
-  );
+  const [result] = _eval(tree1, defaultEnv, code1);
+  const [type] = infer(tree1, defaultTypeEnv, code1);
 
   if (
     !equal(
@@ -91,4 +101,8 @@ test("Record access and application", () => {
     return serialise(diff(tree1, tree2));
   if (result !== 2)
     return `Expected 2 but got ${result}`;
+  if (type !== numberType)
+    return `Expected Number but got ${stringify(
+      type
+    )}`;
 });
