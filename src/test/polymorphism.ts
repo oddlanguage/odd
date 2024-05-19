15aeeb7601a471279a4ebd19aa8ab4790e2c42c5
maanlamp <32102935+maanlamp@users.noreply.github.com>
2024-05-20 01:05:26 +0200

    Disable unimplemented polymorphism test

--- src/test/polymorphism.ts
+++ src/test/polymorphism.ts
@@ -1,15 +1,10 @@
-import _eval from "../core/eval.js";
-import parse, {
-  defaultEnv,
-  nothing,
-} from "../core/odd.js";
+import parse from "../core/odd.js";
 import test from "../core/test.js";
 import {
   defaultTypeEnv,
   infer,
   stringify,
 } from "../core/type.js";
-import { showOddValue } from "../core/util.js";
 
 test("The type of a class expression is itself", () => {
   const code = `class Eq a where (==) : a -> a -> Boolean`;
@@ -46,24 +41,25 @@ test("Class expressions extend the type environment", () => {
   }
 });
 
-test("Instance expressions create new values for each member", () => {
-  const code = `instance Eq a where (==) = nothing`;
-  const [value, , env] = _eval(
-    parse(code),
-    defaultEnv,
-    code
-  );
-  if (value !== nothing) {
-    throw `Instance expression yielded a non-nothing value: "${showOddValue(
-      value
-    )}".`;
-  }
-  if (!env["=="]) {
-    throw `Environment does not contain "==" function.`;
-  }
-  if (typeof env["=="] !== "function") {
-    throw `Expected type of "==" to be "(Eq a) => a -> a -> Boolean" but got "${typeof env[
-      "=="
-    ]}".`;
-  }
-});
+// TODO: Instances are not implemented yet
+// test("Instance expressions create new values for each member", () => {
+//   const code = `instance Eq a where (==) = nothing`;
+//   const [value, , env] = _eval(
+//     parse(code),
+//     defaultEnv,
+//     code
+//   );
+//   if (value !== nothing) {
+//     throw `Instance expression yielded a non-nothing value: "${showOddValue(
+//       value
+//     )}".`;
+//   }
+//   if (!env["=="]) {
+//     throw `Environment does not contain "==" function.`;
+//   }
+//   if (typeof env["=="] !== "function") {
+//     throw `Expected type of "==" to be "(Eq a) => a -> a -> Boolean" but got "${typeof env[
+//       "=="
+//     ]}".`;
+//   }
+// });
